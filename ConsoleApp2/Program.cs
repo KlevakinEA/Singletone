f919c5bf7c959a51fc6d2aacc75004516ad956b9
klevakinea <klevakinea@309D-7>
2024-09-23 16:20:05 +0700

    Доделатью

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+
+class Servers
+{
+    static readonly Servers uniqueInstance = new Servers();
+    string[] servers = new string[0];
+    private Servers() { }
+    public static Servers Instance() { return uniqueInstance; }
+    public bool Add(string address)
+    {
+        if (servers.Contains(address) || (!address.StartsWith("http") && !address.StartsWith("https"))) return false;
+        else
+        {
+            servers.Append(address);
+            return true;
+        }
+    }
+}
